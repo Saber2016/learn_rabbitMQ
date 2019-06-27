62d63a05d77aa4cbbc6fc5f732e04557d2262af9
秦元龙 <956441594@qq.com>
2019-06-27 19:10:33 +0800

    message test
    可执行文件，包括发送和接收，当然你需要先安装rabbitmq。

---
+++ rabbitmq_csharp/Receive/Receive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //和发送端一样，仍然需要链接服务器
+            var factory=new ConnectionFactory{ HostName="localhost"};
+            using(var connection=factory.CreateConnection())
+            using( var channel=connection.CreateModel())
+            {
+                channel.QueueDeclare(queue:"hello",
+                durable:false,
+                exclusive:false,
+                autoDelete:false,
+                arguments:null);
+
+                var consumer=new EventingBasicConsumer(channel);
+                consumer.Received+=(ModuleHandle,ea)=>
+                {
+                    var body=ea.Body;
+                    var message=Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue:"hello", autoAck:true, consumer:consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
